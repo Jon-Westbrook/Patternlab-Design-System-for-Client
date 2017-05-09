cb8e31db58f358787decff7b422821fb2581e137
Jonathon <jwestbrook@vsapartners.com>
2017-05-09 00:17:28 -0400

    Mega-menu basics

--- source/js/custom.js
+++ source/js/custom.js
@@ -8,6 +8,11 @@ $(document).ready(function() {
     $('#search-module').removeClass('search-module--open');
   });
 
+  // Global Search Red Underline
+  document.getElementById("search-module-input").onkeyup = function() {
+    document.getElementById("highlight").innerHTML = this.value;   
+  };
+
 });
 
 
